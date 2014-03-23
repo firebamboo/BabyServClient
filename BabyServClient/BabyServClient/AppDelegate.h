7581e328982c133fe409cdadcba9db2382f9893f
jinzs <jzs@localhost.(none)>
2014-03-23 17:31:18 +0800

    first

---
+++ BabyServClient/BabyServClient/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BabyServClient
+//
+//  Created by jinzs on 14-3-23.
+//  Copyright (c) 2014年 jinzs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BabyServClient/BabyServClient/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BabyServClient
+//
+//  Created by jinzs on 14-3-23.
+//  Copyright (c) 2014年 jinzs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
